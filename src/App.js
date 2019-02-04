b32710227e7c1b1d82b197959466526797d45567
Sviatoslav Slynchuk <slava@zoomdata.com>
2019-02-04 15:34:48 +0200

    User Board - main dependancies was installed. Basic app container was created. ESLint up and running.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars, no-undef, class-methods-use-this */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="main-application-container">
+
+            </div>
+        );
+    }
+}
+
+App.propTypes = {};
+
+export default hot(module)(App);
